c3d80be08e5ce4b6fcfe41e64188f6f854474f0a
Puri Sahib <61573288+purisahib@users.noreply.github.com>
2021-10-01 17:46:27 +0530

    Add files via upload

---
+++ InputController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField] private SimpleCharacterController charController;
+
+    void Awake()
+    {
+        charController = GetComponent<SimpleCharacterController>();
+    }
+
+    private void Update()
+    {
+        // Get input values
+        int vertical = Mathf.RoundToInt(Input.GetAxis("Vertical"));
+        int horizontal = Mathf.RoundToInt(Input.GetAxis("Horizontal"));
+        bool jump = Input.GetKey(KeyCode.Space);
+
+        charController.ForwardInput = vertical;
+        charController.TurnInput = horizontal;
+        charController.JumpInput = jump;
+    }
+}
---
+++ SimpleCharacterController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacterController : MonoBehaviour
+{
+    [Tooltip("Maximum slope the character can jump on")]
+    [Range(5f, 60f), SerializeField] private float slopeLimit = 45f;
+    [Tooltip("Move speed in meters/second")]
+    [SerializeField] private float moveSpeed = 3f;
+    [Tooltip("Turn speed in degrees/second, left (+) or right (-)")]
+    [SerializeField] private float turnSpeed = 300;
+    [Tooltip("Whether the character can jump")]
+    [SerializeField] private bool allowJump = false;
+    [Tooltip("Upward speed to apply when jumping in meters/second")]
+    [SerializeField] private float jumpSpeed = 6f;
+
+    public bool IsGrounded { get; private set; }
+    public float ForwardInput { get; set; }
+    public float TurnInput { get; set; }
+    public bool JumpInput { get; set; }
+
+    [SerializeField] private new Rigidbody rigidbody;
+    [SerializeField] private CapsuleCollider capsuleCollider;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGrounded();
+        ProcessActions();
+    }
+
+
+    /// <summary>
+    /// Checks whether the character is on the ground and updates <see cref="IsGrounded"/>
+    /// </summary>
+    private void CheckGrounded()
+    {
+        IsGrounded = false;
+        float capsuleHeight = Mathf.Max(capsuleCollider.radius * 2f, capsuleCollider.height);
+        Vector3 capsuleBottom = transform.TransformPoint(capsuleCollider.center - Vector3.up * capsuleHeight / 2f);
+        float radius = transform.TransformVector(capsuleCollider.radius, 0f, 0f).magnitude;
+
+        Ray ray = new Ray(capsuleBottom + transform.up * .01f, -transform.up);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, radius * 5f))
+        {
+            float normalAngle = Vector3.Angle(hit.normal, transform.up);
+            if (normalAngle < slopeLimit)
+            {
+                float maxDist = radius / Mathf.Cos(Mathf.Deg2Rad * normalAngle) - radius + .02f;
+                if (hit.distance < maxDist)
+                    IsGrounded = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Processes input actions and converts them into movement
+    /// </summary>
+    private void ProcessActions()
+    {
+        // Turning
+        if (TurnInput != 0f)
+        {
+            float angle = Mathf.Clamp(TurnInput, -1f, 1f) * turnSpeed;
+            transform.Rotate(Vector3.up, Time.fixedDeltaTime * angle);
+        }
+
+        // Movement
+        Vector3 move = transform.forward * Mathf.Clamp(ForwardInput, -1f, 1f) *
+            moveSpeed * Time.fixedDeltaTime;
+        rigidbody.MovePosition(transform.position + move);
+
+        // Jump
+        if (JumpInput && allowJump && IsGrounded)
+        {
+            rigidbody.AddForce(transform.up * jumpSpeed, ForceMode.VelocityChange);
+        }
+    }
+    private void JumpPlayer () {
+        
+    }
+}
